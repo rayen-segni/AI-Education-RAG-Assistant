fd1243b96687b638c789bd03dd3c42684f6a34a6
rayen <snrayen8@gmail.com>
2026-08-10 11:23:58 +0100

    Prepare project structure to add API layer

---
+++ app/config.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# Dynamically resolve project root directory (app/config.py -> app/ -> root)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+class Settings(BaseSettings):
+    # PostgreSQL Configuration
+    DB_NAME: str = "postgres"
+    DB_USER: str = "postgres"
+    DB_PASS: str = "rayen"
+    DB_HOST: str = "127.0.0.1"
+    DB_PORT: int = 5432
+    
+    # Local Ollama AI Engine Settings
+    OLLAMA_URL: str = "http://localhost:11434"
+    EMBEDDING_MODEL: str = "nomic-embed-text:latest"
+    LARGE_LANGUAGE_MODEL: str = "qwen2.5:0.5b"
+
+    # Compute PostgreSQL DSN connection string
+    @computed_field
+    def database_url(self) -> str:
+        return f"dbname={self.DB_NAME} user={self.DB_USER} password={self.DB_PASS} host={self.DB_HOST} port={self.DB_PORT}"
+
+    # Tell Pydantic to read directly from root .env file
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+settings = Settings()
---
+++ app/services/llm_service.py
@@ -0,0 +1,16 @@
+import httpx
+from config import settings
+
+async def query_llm(payload: dict):
+    
+    async with httpx.AsyncClient(timeout=90.0) as client:
+        
+        response = await client.post(
+            f"{settings.OLLAMA_URL}/api/chat",
+            json=payload
+        )
+        
+        response.raise_for_status() 
+        return response.json()
+        
+
