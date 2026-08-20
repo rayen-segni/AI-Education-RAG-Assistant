2c805388cf3cd0515d7d003dfa2bea348b7b6f6f
rayen <snrayen8@gmail.com>
2026-08-20 16:03:12 +0100

    Add sources to search

--- app/routers/search.py
+++ app/routers/search.py
@@ -25,17 +25,20 @@ async def search_documents(payload: SearchRequest):
         threshold=payload.threshold,
         )
 
+    sources = [chunk[4]["source"] for chunk in chunks_rows if "source" in chunk[4]]
+    subjects = [chunk[4]["subject"] for chunk in chunks_rows if "subject" in chunk[4]]
+
     final_chunks = await reranker.reranker(
         payload.query, 
         [chunk[2] for chunk in chunks_rows]
         )
 
-    
-
     response = {
         "query": payload.query,
         "filters": filters,
-        "result": chunks_rows
+        "result": chunks_rows,
+        "sources": list(dict.fromkeys(sources)),
+        "subjects": list(dict.fromkeys(subjects))
     }
 
     return response
